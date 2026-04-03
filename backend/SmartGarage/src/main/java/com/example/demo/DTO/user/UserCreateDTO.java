07d6eb8a41b3bbe6f44ef56e41a92a5ca99d6e00
Emanuil Pavlov <stu2101681090@uni-plovdiv.bg>
2026-04-03 15:43:09 +0300

    Last Stable version

--- backend/SmartGarage/src/main/java/com/example/demo/DTO/user/UserCreateDTO.java
+++ backend/SmartGarage/src/main/java/com/example/demo/DTO/user/UserCreateDTO.java
@@ -1,15 +1,34 @@
 package com.example.demo.DTO.user;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import com.example.demo.DTO.RoleDTO;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
+import org.checkerframework.common.aliasing.qual.Unique;
 
 @Builder
 public record UserCreateDTO(
         @NotNull
         @Size(min = 5, max = 50, message = "The name should be between 5 and 50 symbols")
-        private String name,
+        String name,
 
+        @NotNull
+        @Size(min = 2, max = 20, message = "Username should be between 2 and 20 symbols")
+        String username,
 
+        @Unique
+        @Email(message = "Email should be valid")
+        @NotEmpty(message = "Email cannot be empty")
+        String email,
+
+        @Unique
+        @Pattern(regexp = "\\d{10}", message = "Phone number must be exactly 10 digits")
+        String phone,
+
+        @NotNull
+        @Size(min = 10, max = 40, message = "Address should be between 10 and 40 symbols")
+        String address,
+
+        @NotNull
+        RoleDTO role
 ) {
 }
